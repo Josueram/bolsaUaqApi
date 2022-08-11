023927a50ba09f1fc49c2813ba18b1964bef8b04
Josue Ramirez <josueramirez0110@gmail.com>
2022-08-11 15:28:09 -0500

    a

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('heroku_6a07c725fd6a825', 'b6e3e3f0bb497c', 'd8c95657', {
+const db = new Sequelize('Tables', 'b6e3e3f0bb497c', 'd8c95657', {
     host: 'us-cdbr-east-06.cleardb.net',
     dialect: 'mysql',
     // user: 'b6e3e3f0bb497c',
     // password: 'd8c95657',
-    // database: 'heroku_6a07c725fd6a825',
+    database: 'heroku_6a07c725fd6a825',
     port: 3308,
     define: {
         timestamps: false
